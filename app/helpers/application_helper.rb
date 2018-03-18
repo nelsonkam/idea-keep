f52d581935658d5a1fcfaad9d8a9d0704177c336
Nelson Kamga <kamganelson@gmail.com>
2018-03-18 19:04:27 +0100

    Redesign devise auth views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def resource_name
+    :user
+  end
+
+  def resource_class 
+     User 
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
+  def minimum_password_length
+    @minimum_password_length = User.password_length.min
+  end
 end
